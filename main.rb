d3a5640789c8e0c02edc4397aece1a595be3fe7e
ongaeshi <ongaeshi0621@gmail.com>
2020-01-24 02:27:02 +0900

    feat: walk demo

--- main.rb
+++ main.rb
@@ -1,20 +1,29 @@
 require "./raylib"
 include Raylib
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 450
+SCREEN_WIDTH = 320
+SCREEN_HEIGHT = 320
 
 window(SCREEN_WIDTH, SCREEN_HEIGHT, "fude") do
 	set_target_fps(60)
 
+	sprite = load_texture("resources/sprite.png")
+	x = 0
+	xa = 0
+
 	until window_should_close do
 		# Update
-		# TODO: Update your variables here
+		x += 1
+		x = 0 if x > 300
+
+		xa += 1
 
 		# Draw
 		draw do
-			clear_background(RAYWHITE)
-			draw_text("Hello, World!", 190, 200, 20, LIGHTGRAY);
+			clear_background(Color.init(77, 253, 252, 255))
+			# draw_texture(sprite, 15, 40, WHITE)
+			draw_texture_rec(sprite, Rectangle.init(16 * 2, 16 * ((xa / 20).to_i % 2 + 1), 16, 16), Vector2.init(x, 40), WHITE)
+			# draw_text("Hello, World!", 190, 200, 20, LIGHTGRAY);
 		end
 	end
 end
