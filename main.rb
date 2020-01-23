8c778c2e517c83be06ed9ebf6b11379a4e93b442
ongaeshi <ongaeshi0621@gmail.com>
2020-01-24 02:31:16 +0900

    feat: Change to transparent png

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@ require "./raylib"
 include Raylib
 
 SCREEN_WIDTH = 320
-SCREEN_HEIGHT = 320
+SCREEN_HEIGHT = 200
 
 window(SCREEN_WIDTH, SCREEN_HEIGHT, "fude") do
 	set_target_fps(60)
@@ -20,10 +20,12 @@ window(SCREEN_WIDTH, SCREEN_HEIGHT, "fude") do
 
 		# Draw
 		draw do
-			clear_background(Color.init(77, 253, 252, 255))
+			clear_background(RAYWHITE)
+			draw_text("Sprite Animation", 10, 10, 20, LIGHTGRAY);
 			# draw_texture(sprite, 15, 40, WHITE)
 			draw_texture_rec(sprite, Rectangle.init(16 * 2, 16 * ((xa / 20).to_i % 2 + 1), 16, 16), Vector2.init(x, 40), WHITE)
-			# draw_text("Hello, World!", 190, 200, 20, LIGHTGRAY);
+			draw_texture_rec(sprite, Rectangle.init(16 * 5, 16 * ((xa / 20).to_i % 2 + 1), 16, 16), Vector2.init(x - 20, 80), WHITE)
+			# 
 		end
 	end
 end
